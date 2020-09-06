f8857e7b5962995d402a9d9a20ce38332b74d7bb
Anand Prakash <anandprakash@sti.com>
2020-09-06 15:20:28 +0530

    v1

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.browserAction.onClicked.addListener(function (tab) {
+    const colors =['red','orange','yellow','green','blue','violet'];
+    const colorPicker =()=>{
+        const randomIndex =Math.floor(Math.random()*colors.length);
+        return colors[randomIndex];
+    }
+
+chrome.tabs.executeScript({
+    code :'document.body.style.backgroundColor=" ' + colorPicker() + ' " ',
+});
+});
+
+
